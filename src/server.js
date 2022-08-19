6b09073ce5e9d0c2717839bf5cf754e703402c47
Tae Min Jung <71391013+aquisto535@users.noreply.github.com>
2022-08-19 20:06:36 +0900

    install pug and morgan

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const PORT = 4000;
 
 const app = express();
 
+app.set("view engine", "pug");
+app.use(logger);
+
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
