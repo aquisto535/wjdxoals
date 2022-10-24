9417cb00817a38a0702111eb43a85c09e312e6ef
Tae Min Jung <outopia3@gmail.com>
2022-10-24 21:59:47 +0900

    recap How to use pug

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import rootRouter from "./routers/rootRouter";
 
-const PORT = 4000;
+const PORT = 7000;
 
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views"); //pug 설치할 때 같이 있어야 함.
 app.use(logger);
+
+app.use("/", rootRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
